abd0d6f80dde8aeabdbdfa56ce244c68ce144c4f
James Ubiadas <jubiadas@csu.fullerton.edu>
2023-11-01 20:45:15 -0700

    Fixed linter error and header error. This is my first git commit.

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Adam Secrest
+// adam.secrest@csu.fullerton.edu
+// @Secrest2904
+// Partners: @jubiadas004
 
 #include <iostream>
 #include <string>
@@ -6,26 +9,24 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() <= 1) {
+    std::cout << "Error: you must supply at least one number."
+              << ".\n";
+    return 1;
+  }
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  double sum = 0.0;
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+  for (const std::string& argument : arguments) {
+    if (argument == arguments[0]) {
+      continue;
+    }
+    double number = std::stof(argument);
+    sum += number;
+  }
 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  double average = sum / static_cast<double>(arguments.size() - 1);
+  std::cout << "average = " << average << "\n";
 
   return 0;
 }
